1b052157eaf371c37453fbcafbc2b3b3266d7155
Jack Franks <jonathan.franks@breaktech.com>
2025-06-26 11:26:26 -0500

    Disable recaptcha to check user password reset

--- web/modules/custom/testable/tests/src/ExistingSite/TestableCaptchaTest.php
+++ web/modules/custom/testable/tests/src/ExistingSite/TestableCaptchaTest.php
@@ -11,6 +11,14 @@ class TestableCaptchaTest extends ExistingSiteBase {
   use CaptchaTrait;
 
   /**
+   * {@inheritDoc}
+   */
+  public function tearDown(): void {
+    $this->enableAllRecaptchas();
+    parent::tearDown();
+  }
+
+  /**
    * Tests that the login form does not have a captcha.
    *
    * @throws \Behat\Mink\Exception\ExpectationException
@@ -39,6 +47,25 @@ class TestableCaptchaTest extends ExistingSiteBase {
   public function testRegisterHasCaptcha() {
     $this->drupalGet('/user/register');
     $this->assertCaptchaExists();
+  }
+
+
+  /**
+   * Tests the reset password function.
+   */
+  public function testForgotPasswordExistingAccount() {
+    $this->disableRecaptcha('user_pass');
+    $user = $this->createUser([], 'user@mailinator.com', FALSE, [
+      'mail' => 'user@mailinator.com',
+      'field_first_name' => 'Existing',
+      'field_last_name' => 'User',
+      'pass' => 'pass1234!',
+    ]);
+
+    $this->drupalGet('/user/password');
+    $this->getCurrentPage()->fillField('Username or email address', $user->getEmail());
+    $this->getCurrentPage()->pressButton('Submit');
+    $this->assertSession()->pageTextContains('If user@mailinator.com is a valid account, an email will be sent with instructions to reset your password.');
   }
 
 }
